97f95473513080cf6685f64fad63bfa4aec8150b
John Christian Tan <jchristian.abante@gmail.com>
2023-07-20 18:49:15 +0800

    Featured: initialize capstone

---
+++ README.md
@@ -0,0 +1,45 @@
+# <h1 align="center">Capstone Project Guide for Batch RP1 🚀</h1>
+
+### Authored By: ⚓ John Christian Tan ⚓
+
+<br>
+
+# Outline
+
+## Initial Setup
+
+### Frontend / Client-side
+
+### Backend / Server-side
+
+### Database
+
+Contributors: Batch RP1 Learners 🌱
+
+<span style="color:#b3cde0; font-weight:bold; font-size:1.6em;">🤖 Technology Used </span>
+
+### <span style="color:#4b8333; font-weight:bold; font-size:1.6em;">Frontend</span>
+
+#### React
+
+#### Axios / Fetch API
+
+### <span style="color:#db4c4c; font-weight:bold; font-size:1.6em;">Backend</span>
+
+#### Node
+
+#### Express
+
+#### Sequelize ORM
+
+### <span style="color:#00FFFF; font-weight:bold; font-size:1.6em;">Database</span>
+
+#### PostgreSQL
+
+### <span style="color:#cbefef; font-weight:bold; font-size:1.6em;">DevTools</span>
+
+#### Code Editor: VSCode
+
+#### API Tester: Postman
+
+#### Deployment: Vercel / Render
---
+++ migration.sql
@@ -0,0 +1,62 @@
+CREATE TABLE "users" (
+  "user_id" integer,
+  "user_name"  character varying(255) not null,
+  "password"  character varying(255),
+  "user_type"  character varying(255),
+  CONSTRAINT users_pkey PRIMARY KEY (user_id),
+  CONSTRAINT unique_email UNIQUE (user_name)
+);
+-- SEQUENCE FOR AUTO INCREMENT
+CREATE SEQUENCE IF NOT EXISTS public.users_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1
+    OWNED BY users.user_id;
+
+-- 
+ALTER TABLE users ALTER COLUMN user_id SET DEFAULT nextval('users_id_seq'::regclass);
+
+CREATE TABLE "branches" (
+  "branch_code" varying character (255),
+  "branch_name" varying character (255),
+  "percent_share" double (2, 0),
+  PRIMARY KEY ("branch_code")
+);
+
+CREATE TABLE "transactions" (
+  "transaction_id" varying character (255),
+  "voucher_number" integer,
+  "branch_code" varying character (255),
+  "customer_id" integer,
+  "date" datetime,
+  "total_amount" double (10, 2),
+  PRIMARY KEY ("transaction_id")
+);
+
+CREATE TABLE "availed_services" (
+  "a_service_id" integer     ,
+  "therapist_id" integer,
+  "service_id" integer,
+  "discount_code" varying character (255),
+  "tip" Type,
+  PRIMARY KEY ("a_service_id")
+);
+CREATE TABLE "services" (
+  "service_id" integer,
+  "service_name" varying character (255),
+  "service_type" varying character (255),
+  "price" double (10, 2),
+  "minutes" integer,
+  "commission" double (10, 2),
+  PRIMARY KEY ("service_id")
+);
+
+
+CREATE TABLE "discounts" (
+  "discount_code" varying character (255),
+  "discount_description" varying character (255),
+  "percentage" double (2, 0),
+  PRIMARY KEY ("discount_code")
+);
